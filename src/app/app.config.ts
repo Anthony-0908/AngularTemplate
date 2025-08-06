8e9d3e97b6ef8b38d43a2fffc50911ce605e89b6
Anthony-0908 <anthonyyuvega09@gmail.com>
2025-08-06 20:47:23 +0800

    added ngrx

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,12 +3,13 @@ import { provideRouter } from '@angular/router';
 
 
 import { routes } from './app.routes';
+import { provideStore } from '@ngrx/store';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-
-  ]
+    provideStore()
+]
 };
