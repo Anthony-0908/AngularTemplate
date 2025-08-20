e01fdfc264dc9a8affe2af71fa25583010f6c28c
Anthony-0908 <anthonyyuvega09@gmail.com>
2025-08-20 19:04:36 +0800

    app routing is now working

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { provideRouter } from '@angular/router';
+import { Dashboard } from './dashboard/dashboard';
+export const routes: Routes = [
+  {
+    path:'dashboard',
+    loadComponent: () => import('./dashboard/dashboard').then(m => m.Dashboard)
+  }
+];
